b69c17d21021d1b55cbff124c9d1a24eb0f58830
MD. RAKIBUL HASAN <rakibulmrh4bd@gmail.com>
2024-02-13 13:53:14 +0600

    Lab 3

---
+++ Lab Task 3/test.js
@@ -0,0 +1,17 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    if (req.url === '/home') {
+        res.end('<h2>B</h2>');
+    }
+    if (req.url === '/login') {
+        res.end('<h2>Lets Login</h2>');
+    }
+}
+);
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
+);
